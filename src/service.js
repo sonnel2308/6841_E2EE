ae998777e59ab2f4a7f011e10496d382e8480045
Nelson Lin <nelson.lin2308@gmail.com>
2023-11-02 13:15:30 +1100

    database functions

--- src/service.js
+++ src/service.js
@@ -4,19 +4,46 @@ import fs from 'fs';
 const database = {};
 
 // Initialise database.json.
-access("../database.json", constants.F_OK)
-.then((data) => {
-    console.log("Accessing database.json");
-    fs.readFile("../database.json", (error, content) => {
-        let data = JSON.parse(content);
-        console.log(data);
-    });
-})
-.catch((error) => {
-    console.log("Creating database.json");
-    database["users"] = [];
-    database["messages"] = [];
-    console.log(database);
+const initDatabase = async () => {
+    try {
+        await access("../database.json", constants.F_OK);
+        console.log("Accessing database.json");
+        const content = fs.readFileSync("../database.json", 'utf-8');
+        const parsedDatabase = JSON.parse(content);
+        Object.assign(database, parsedDatabase);
+        // console.log("database:", database);
+    } catch (error) {
+        console.log("Creating database.json");
+        database["users"] = [];
+        database["messages"] = [];
+        // console.log("database:", database);
 
+        fs.writeFileSync("../database.json", JSON.stringify(database, null, 4));
+    }
+}
+
+// Add user to database.
+export const createUser = async (name) => {
+    await initDatabase();
+    if (database["users"].includes(name)) {
+        console.log(name, "already exists");
+        throw new Error(name, "already exists");
+    }
+    database["users"].push(name);
     fs.writeFileSync("../database.json", JSON.stringify(database, null, 4));
-});
+}
+
+// Send message between two users to database.
+export const sendMessage = async (sender, receiver, message) => {
+    await initDatabase();
+    const msgBody = {
+        sender,
+        receiver,
+        message
+    }
+    database["messages"].push(msgBody);
+    fs.writeFileSync("../database.json", JSON.stringify(database, null, 4));
+}
+
+createUser("abcasd");
+// sendMessage("abc", "xyz", "hi guys");
